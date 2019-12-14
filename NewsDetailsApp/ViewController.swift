bec498c755a80d65cfc4431409222842408dca93
T Sydulu <tsydulu9@gmail.com>
2019-12-14 18:28:35 +0530

    created table view

--- NewsDetailsApp/ViewController.swift
+++ NewsDetailsApp/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    
+    
+    var newsTableView:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        creatTableView()
     }
-
+    
+    //creating tableview
+    func creatTableView(){
+        newsTableView = UITableView(frame: CGRect(x: 0, y: 50, width: view.frame.width, height: view.frame.height-50), style: .plain)
+        view.addSubview(newsTableView)
+        
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+        
+        // registerig tableview
+        
+        newsTableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
+        
+    }
+    
+    
+    // tableview delegate and datasource func's
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
 
