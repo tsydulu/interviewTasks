c3254032a02a321cc35972057cd3bf7aac810955
T Sydulu <tsydulu9@gmail.com>
2019-12-14 17:55:14 +0530

    Initial Commit

---
+++ NewsDetailsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsDetailsApp
+//
+//  Created by SYDULU THAPPETLA on 14/12/19.
+//  Copyright © 2019 SYDULU THAPPETLA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NewsDetailsAppTests/NewsDetailsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewsDetailsAppTests.swift
+//  NewsDetailsAppTests
+//
+//  Created by SYDULU THAPPETLA on 14/12/19.
+//  Copyright © 2019 SYDULU THAPPETLA. All rights reserved.
+//
+
+import XCTest
+@testable import NewsDetailsApp
+
+class NewsDetailsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
